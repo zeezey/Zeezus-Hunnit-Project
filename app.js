52320d1a5df89e59d3ea320beb9aecde84a6a10a
Arun Selvaraj <zeezey@Aruns-MacBook-Pro.local>
2016-01-14 13:43:05 -0700

    22

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-var app = angular.module('health', []);
+var app = angular.module('health', ['chart.js']);
+angular.module("app", ["chart.js"]).controller("PieCtrl", function ($scope) {
+  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+  $scope.data = [300, 500, 100];
+});
